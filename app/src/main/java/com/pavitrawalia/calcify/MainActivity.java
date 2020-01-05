188930530ea891f9a7f7ea16ea78f9a9e4ec2a46
hacker <agarwalbinay19@gmail.com>
2020-01-05 18:54:05 +0530

    remove floating point button

--- app/src/main/java/com/pavitrawalia/calcify/MainActivity.java
+++ app/src/main/java/com/pavitrawalia/calcify/MainActivity.java
@@ -31,12 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
