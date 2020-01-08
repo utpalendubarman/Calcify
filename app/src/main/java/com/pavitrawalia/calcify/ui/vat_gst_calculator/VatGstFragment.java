2c8ff9ffc50b2a8cbf9e7f973b9517e8105492a3
Pavitra Walia <waliapavitra@gmail.com>
2020-01-08 21:17:55 +0530

    Revert "Created User Interface for VAT And GST"

--- app/src/main/java/com/pavitrawalia/calcify/ui/vat_gst_calculator/VatGstFragment.java
+++ app/src/main/java/com/pavitrawalia/calcify/ui/vat_gst_calculator/VatGstFragment.java
@@ -1,62 +1,35 @@
 package com.pavitrawalia.calcify.ui.vat_gst_calculator;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import com.pavitrawalia.calcify.R;
 
 public class VatGstFragment extends Fragment {
 
+    private VatGstViewModel homeViewModel;
 
-    private int contentView;
-    private Button id1;
-    private Button id2;
-
-
-    /*@Override
-     public   void onCreateView(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_vat_gst);
-         id1= Button.findViewById(R.id.VatButton);
-        id1.setOnClickListener(new View.OnClickListener() {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(VatGstViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_vat_gst, container, false);
+        final TextView textView = root.findViewById(R.id.text_vat_gst);
+        homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(VatGstFragment.this,VatCalulator.class));
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
             }
         });
-        id2= Button.findViewById(R.id.GstButton);
-        id2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(VatGstFragment.this,GstCalculator.class));
-            }
-        });*/
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_vat_gst, container, false);
-        id1= rootView.findViewById(R.id.VatButton);
-        id1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(VatGstFragment.this.getActivity(), VatCalulator.class);
-                startActivity(intent);
-            }
-        });
-        id2= rootView.findViewById(R.id.GstButton);
-        id2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(VatGstFragment.this.getActivity(),GstCalculator.class));
-            }
-
-    });
-        return rootView;
-    }}
-
-
-
+        return root;
+    }
+}
